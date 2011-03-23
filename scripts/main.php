679c980854c9b2bb54d8af349bb8e46db28d9459
Mendel <mendelsongusmao@gmail.com>
2011-03-23 18:49:26 -0300

    readme and code separation

---
+++ scripts/main.php
@@ -0,0 +1,25 @@
+<?php
+
+    error_reporting (E_ERROR | E_PARSE);
+
+    function main () {
+
+        global $argv;
+
+        $params = explode("#", $argv[1]);
+
+        if ($function = $params[0])
+	    die(
+                function_exists($function)?
+                    call_user_func_array(
+                        $function,
+                        1 == count($params)?
+			    array()
+                            : explode(";", $params[1])
+                    )
+		    : "[php] function '{$params[0]}' doesn't exists\n"
+                );
+
+    }
+
+?>
