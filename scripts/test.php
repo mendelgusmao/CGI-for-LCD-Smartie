c6e4a87591d61e2f9032f821b0aabfdc50139479
MendelGusmao <mendelsongusmao@gmail.com>
2011-04-02 23:33:19 -0300

    several modifications:
    - function2 - display PHP version <full php -v>
    - automatically include main.php in every executed script. so, there is no need to include main.php at the beginning of the script. its use still remains optional by calling main() at the end of the script [or not]
    - included a new example script called coretemp, that extracts data from core temp's log and show them on the screen. the avaliable functions are:
    	temp - the actual temperature of the processor
    	low_temp - the lowest temperature registered since core temp's start
    	high_temp - the highest temperature
    	core_load - the processor load [ I know it's native in LCD Smartie, but it's just an example]
    	speed - actual clock speed of the processor [ not the nominal speed displayed by $CPUSpeed ]
    - and the biggest change: to use a different php.ini, lighter than the original, to give a boost in the php process start.
    - also, the configuration of the plugin, previously made in config.ini of LCD Smartie now is done in php.ini

---
+++ scripts/test.php
@@ -0,0 +1,19 @@
+<?php
+
+    require "main.php";
+
+    # $dll(php,1,test,hello)
+    function hello () {
+        echo "Hello, world!";
+    }
+
+    # $dll(php,1,test,do_some_math#3;2;1)
+    function do_some_math ($param1, $param2, $param3) {
+        echo $param1 * $param2 + $param3;
+    }
+
+    main();
+
+    echo "normal execution";
+
+?>
