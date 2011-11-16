694d768e0542cb4d05881aa0cdf71b6984633d72
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-11-16 19:39:23 -0200

    Created directory for bootstraps (the old main functions) and a bootstrap for Ruby.

---
+++ scripts/bootstraps/!.rb
@@ -0,0 +1,19 @@
+def main
+  
+  unless ARGV[0].nil?
+  
+    params = ARGV[0].split "#"
+
+    if function = params[0]
+      if respond_to? function.to_sym
+	puts send(function, *(params.size == 1 ? {} : params[1].to_s.split(";")))
+      else
+	puts "[CGI4LCD] Function '" + function + "' not found"
+      end
+    end
+
+  end  
+    
+  Kernel::exit
+  
+end
