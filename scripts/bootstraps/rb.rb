411c1b048fdc467875391e2ac9baf881114e13c4
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-11-17 15:25:37 -0200

    Minor fixes. Added test for non-existent function

--- scripts/bootstraps/rb.rb
+++ scripts/bootstraps/rb.rb
@@ -6,9 +6,9 @@ def main()
 
     if function = params[0]
       if private_methods.include? function.to_sym
-        puts send(function, *(params.size == 1 ? {} : params[1].split(";")))
+        print send(function, *(params.size == 1 ? {} : params[1].split(";")))
       else
-        puts "[CGI4LCD] Function '#{function}' not found"
+        print "[CGI4LCD] Function '#{function}' not found\n"
       end
     end
 
