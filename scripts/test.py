104ca655986412f19322f66912b0c0b3189c0118
MendelGusmao <mendelsongusmao@gmail.com>
2011-11-17 15:25:37 -0200

    Minor fixes. Added test for non-existent function

--- scripts/test.py
+++ scripts/test.py
@@ -1,12 +1,15 @@
 from bootstraps.py import main
 
+# $dll(cgi,1,test.rb,hello)
 def hello():
-    return "Hello, world!"
+    return "Hello, world!\n"
 
+# $dll(cgi,1,test.rb,say_hi#Mendel)
 def say_hi(who):
-    return "Hi, %s!" % who
+    return "Hi, %s!\n" % who
     
+# $dll(cgi,1,test.rb,do_some_math#3;2;1)
 def do_some_math(a, b, c):
-    return "%s" % (int(a) * int(b) + int(c))
+    return "%s\n" % (int(a) * int(b) + int(c))
     
 main()
