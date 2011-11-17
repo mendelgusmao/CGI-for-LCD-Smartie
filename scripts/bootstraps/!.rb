228c6bc6fc314e76d6cc18be593941e2320ab7b0
MendelGusmao <mendelsongusmao@gmail.com>
2011-11-17 10:40:13 -0200

    Update scripts/bootstraps/!.rb

--- scripts/bootstraps/!.rb
+++ scripts/bootstraps/!.rb
@@ -5,6 +5,7 @@ def main
     params = ARGV[0].split "#"
 
     if function = params[0]
+      # Not working - how to check if the function exists?
       if Kernel.respond_to? function.to_s
         puts send(function, *(params.size == 1 ? {} : params[1].to_s.split(";")))
       else
