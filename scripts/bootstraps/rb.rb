ce16fd4ebeb6e44a523409af0ada7d7d91428e45
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-11-17 14:56:28 -0200

    Cleaning code

--- scripts/bootstraps/rb.rb
+++ scripts/bootstraps/rb.rb
@@ -8,7 +8,7 @@ def main()
       if self.private_methods.include? function.to_sym
         puts send(function, *(params.size == 1 ? {} : params[1].to_s.split(";")))
       else
-        puts "[CGI4LCD] Function '" + function + "' not found"
+        puts "[CGI4LCD] Function '#{function}' not found"
       end
     end
 
