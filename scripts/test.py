f30ee5ba40ab45ae484ebd4e74f0a5dbe5d774ed
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-11-17 12:32:52 -0200

    Finished Python bootstrap and standardizing tests

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,12 @@
-#Wrong
-#from "bootstraps/!" import main
+from bootstraps.py import main
 
-def foo:
-    return "foobar"
+def hello():
+    return "Hello, world!"
 
+def say_hi(who):
+    return "Hi, %s!" % who
+    
+def do_some_math(a, b, c):
+    return "%s" % (int(a) * int(b) + int(c))
+    
 main()
