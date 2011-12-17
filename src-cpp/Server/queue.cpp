916234ad3e883a651c39ed732dcda9bc03d15b11
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-12-17 15:30:31 -0200

    Server/Server.vcxproj
    First functional version of CGI4LCD server

--- src-cpp/Server/queue.cpp
+++ src-cpp/Server/queue.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "command.cpp"
+#include "runner.cpp"
 #include <map>
 
 #ifndef QUEUE_CPP
@@ -11,9 +12,20 @@ using std::map;
 class queue {
 public:
     void add_function(command &cmd) {
+
         if (_commands.count(cmd.line()) == 0) {
+			// start timer
+		}
+
+		map<string, command>::iterator it = _commands.find(cmd.line());
+
+		if (it == _commands.end()) {
             _commands[cmd.line()] = cmd;
-            // start timer
+			std::cout << "command queued" << std::endl;
+			runner rnr;
+			rnr.run(cmd, "");
+			std::cout << "command runned" << std::endl;
+			std::cout << cmd.response << std::endl;
         }
         else {
             _commands[cmd.line()].cleanup_timer = 0;
@@ -23,6 +35,7 @@ public:
     void run_commands() {
         map<string, command>::iterator it;
         command cmd;
+		runner rnr;
 
         for (it = _commands.begin(); it != _commands.end(); ++it) {
             cmd = it->second;
@@ -34,8 +47,10 @@ public:
                 _commands.erase(it);
             }
             else if (cmd.timer >= cmd.interval) {
-                cmd.execute();
+
+				rnr.run(cmd, "");
                 cmd.timer = 0;
+				std::cout << cmd.response << std::endl;
             }
         }
     }
