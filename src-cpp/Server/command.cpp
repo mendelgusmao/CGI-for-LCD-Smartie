40aa550e0254e63808d86f21318a6d955fbf4a7d
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-01-15 00:09:03 -0200

    First functional version of CGI4LCD's Server

--- src-cpp/Server/command.cpp
+++ src-cpp/Server/command.cpp
@@ -9,17 +9,43 @@ class command {
 public:
     string executable;
 	string arguments;
-	int timeout;
-	int interval;
-	int timer;
-	int cleanup_timer;
+	unsigned int timeout;
+	unsigned int interval;
+	unsigned int timer;
+	unsigned int cleanup_timer;
 	string response;
 	bool is_malformed;
+
+	command() :
+		timer(0),
+		cleanup_timer(0)
+	{}
 
     string line() {
         return executable + " " + arguments;
     }
 
+    void run() {
+
+		char psBuffer[128];
+		FILE *iopipe;
+
+		iopipe = _popen(line().c_str(), "r");
+
+		if (iopipe == NULL) {
+			response = "[CGI4LCD] Error running...";
+		}
+		else {
+			response = "";
+
+			while(!feof(iopipe)) {
+				if(fgets(psBuffer, 128, iopipe) != NULL) {
+					response += string(psBuffer);
+				}
+			}
+			_pclose(iopipe);
+		}
+	}
 };
 
 #endif 
