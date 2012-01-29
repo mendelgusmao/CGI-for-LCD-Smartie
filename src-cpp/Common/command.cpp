7090d95b4c5f6386da6d7899e7e2f69c37eb9bdd
MendelGusmao <mendelsongusmao@gmail.com>
2012-01-29 20:56:50 -0200

    Common files in server and plugin projects

---
+++ src-cpp/Common/command.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+
+using std::string;
+
+#ifndef COMMAND_CPP
+#define COMMAND_CPP
+
+class command {
+public:
+	string executable;
+	string arguments;
+	unsigned int timeout;
+	unsigned int interval;
+	unsigned int timer;
+	unsigned int cleanup_timer;
+	string response;
+	bool is_malformed;
+
+	command() :
+		timer(0),
+		cleanup_timer(0)
+	{}
+
+	string line() {
+		return executable + " " + arguments;
+	}
+
+	void run() {
+
+		char psBuffer[128];
+		FILE *iopipe;
+
+		iopipe = _popen(line().c_str(), "r");
+
+		if (iopipe == NULL) {
+			response = "[CGI4LCD] Error running...";
+		}
+		else {
+			response = "";
+
+			while(!feof(iopipe)) {
+				if(fgets(psBuffer, 128, iopipe) != NULL) {
+					response += string(psBuffer);
+				}
+			}
+			_pclose(iopipe);
+		}
+	}
+};
+
+#endif 
+
+
+
