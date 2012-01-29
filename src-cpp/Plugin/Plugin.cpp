a4fbf7baacaad8bc6ae8ec093ee3cf152d40b7b9
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-01-29 20:52:34 -0200

    Plugin is almost done

--- src-cpp/Plugin/Plugin.cpp
+++ src-cpp/Plugin/Plugin.cpp
@@ -2,19 +2,14 @@
 #include <windows.h>
 #include <stdlib.h>
 #include "client.h"
+#include "utils.cpp"
 
 #define DLLEXPORT __declspec(dllexport)
-
-
 
 extern "C" DLLEXPORT  void 
 __stdcall  SmartieInit()
 {
-	unsigned int port(0);
-	string ini_file("");
-	string scripts_path("");
-
-	client::start(port, ini_file, scripts_path);
+	client::start();
 }
 
 extern "C" DLLEXPORT  void 
@@ -24,24 +19,26 @@ __stdcall  SmartieFini()
 extern "C" DLLEXPORT  int
 __stdcall  GetMinRefreshInterval()
 {
-	return 300; // 300 ms
+	return 1000; // 300 ms
 }
 
-extern "C" DLLEXPORT  char * 
+extern "C" DLLEXPORT  const char * 
 __stdcall  function1(char *param1, char *param2)
 {
-    return "This is function 1";       
+	string script(param1);
+	string parameters(param2);
+
+	char* response = const_cast<char*>(client::execute(script, parameters).c_str());
+	return response;
 }
 
 extern "C" DLLEXPORT  char * 
 __stdcall  function2(char *param1, char *param2)
 {
-    static char outbuf[1000];
-    static int count;
-    
-    count ++;
-    
-    _itoa_s(count, outbuf, 10);
-    return outbuf;       
+	string script(param1);
+	string parameters(param2);
+
+	char* response = const_cast<char*>(client::execute(script, parameters, true).c_str());
+	return response;
 }
     
