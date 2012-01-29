d443c9e6a13527073c2e9644418a0141999e5048
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-01-29 20:52:34 -0200

    Plugin is almost done

--- src-cpp/Plugin/stdafx.h
+++ src-cpp/Plugin/stdafx.h
@@ -26,5 +26,6 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
+#include <boost/filesystem.hpp>
 
 // TODO: reference additional headers your program requires here
