812d641a0fe7d7e3e77e5ec8b671fc2446405a41
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-01-31 22:44:51 -0200

    Adding sleep between calls

--- src-cpp/Test/main.cpp
+++ src-cpp/Test/main.cpp
@@ -5,7 +5,9 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	client::start();
 	cout << "Response: '" << client::execute("test.php", "hello") << "'" << endl;
+	Sleep(1000);
 	cout << "Response: '" << client::execute("test.php", "say_hi#Mendel") << "'" << endl;
+	Sleep(1000);
 	cout << "Response: '" << client::execute("test.php", "do_some_math#10;20;30") << "'" << endl;
 
 	string input("");
