c9e6478e523f821ad42e4c4063aa29c2f5e23038
MendelGusmao <mendelsongusmao@gmail.com>
2012-01-31 23:02:52 -0200

    Renaming source directory

---
+++ src/Server/stdafx.h
@@ -0,0 +1,31 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <map>
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/filesystem.hpp>
+
+// TODO: reference additional headers your program requires here
