4a18d3d24cb1a3a07a2ec33501e55e045d38915a
MendelGusmao <mendelsongusmao@gmail.com>
2012-01-31 22:41:13 -0200

    Restoring code to hide server window

--- src-cpp/Server/main.cpp
+++ src-cpp/Server/main.cpp
@@ -6,12 +6,12 @@ int main(int argc, char* argv[]) {
 
 	string ini_file(utils::app_path() + "\\..\\scripts\\cgi4lcd.ini");
 	unsigned int port = lexical_cast<unsigned int>(utils::ini_read(ini_file, "cgi4lcd.port", "65432"));
-	//bool show_window = utils::ini_read(ini_file, "cgi4lcd.show_window", "0") == "1";
+	bool show_window = utils::ini_read(ini_file, "cgi4lcd.show_window", "0") == "1";
 
-	//if (!show_window) {
-	//	HWND hWnd = GetConsoleWindow();
-	//	ShowWindow( hWnd, SW_HIDE );
-	//}
+	if (!show_window) {
+		HWND hWnd = GetConsoleWindow();
+		ShowWindow( hWnd, SW_HIDE );
+	}
 
     try {
         boost::asio::io_service io_service;
