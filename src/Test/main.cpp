dfee5570a44017fe0dce4fe9fbdd644164467172
MendelGusmao <mendelsongusmao@gmail.com>
2012-02-02 18:40:39 -0200

    Updated test

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -7,16 +7,28 @@ int _tmain(int argc, _TCHAR* argv[])
 
     cout << "Response: '" << client::execute("test.php", "hello") << "'" << endl;
     Sleep(1000);
+    
     cout << "Response: '" << client::execute("test.php", "say_hi#Mendel") << "'" << endl;
     Sleep(1000);
+    
     cout << "Response: '" << client::execute("test.php", "do_some_math#10;20;30") << "'" << endl;
     Sleep(1000);
+
+    cout << "Response: '" << client::execute("test.php", "not_implemented") << "'" << endl;
+    Sleep(1000);        
+    
     cout << "Response: '" << client::execute("test", "hello") << "'" << endl;
     Sleep(1000);
+    
     cout << "Response: '" << client::execute("test", "say_hi#Mendel") << "'" << endl;
     Sleep(1000);
+    
     cout << "Response: '" << client::execute("test", "do_some_math#10;20;30") << "'" << endl;
+    Sleep(1000);
 
+    cout << "Response: '" << client::execute("test", "not_implemented") << "'" << endl;
+    Sleep(1000);        
+    
     string input("");
     getline(cin, input);
 }
