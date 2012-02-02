32189ee36bbfcb797ffacabf31948dc321af484c
MendelGusmao <mendelsongusmao@gmail.com>
2012-02-02 21:25:31 -0200

    Oops!

--- src/Common/protocol.cpp
+++ src/Common/protocol.cpp
@@ -31,10 +31,11 @@ public:
             if (packet[0] != PROTOCOL_HEADER) {
                 cmd.is_malformed = true;
             }
-            else if (packet[1] == "command" && packet[2] != "")
+            else if (packet[1] == "command" && packet[2] != "") {
                 cmd.is_internal = true;
                 cmd.executable = packet[1];
                 cmd.arguments = packet[2];
+			}
             else {
                 cmd.executable = packet[1];
                 cmd.arguments = packet[2];
