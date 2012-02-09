c08520c1c707f5b649c02bc417a1f60451782da6
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-02-09 02:17:01 -0200

    Changing default value of add_and_run

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     string ini_file(utils::app_path() + "\\..\\scripts\\cgi4lcd.ini");
     unsigned int port = lexical_cast<unsigned int>(utils::ini_read(ini_file, "cgi4lcd.port", "65432"));
     bool show_window = utils::ini_read(ini_file, "cgi4lcd.show_window", "0") == "1";
-    bool add_and_run = utils::ini_read(ini_file, "cgi4lcd.add_and_run", "0") == "1";
+    bool add_and_run = utils::ini_read(ini_file, "cgi4lcd.add_and_run", "1") == "1";
 
     if (!show_window) {
         HWND hWnd = GetConsoleWindow();
