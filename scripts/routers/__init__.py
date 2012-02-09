45dc9061a14845688a400fa5f1acb8793dd63d9e
Mendel Gusmão <mendelsongusmao+github@gmail.com>
2012-02-09 00:57:46 -0200

    Fixing indentation

--- scripts/routers/__init__.py
+++ scripts/routers/__init__.py
@@ -2,15 +2,15 @@ import sys, __main__
 
 def main():
     if (len(sys.argv) > 1):
-	params = sys.argv[1].split("#")
-	function = params[0]
-	
-	if (function):
-	    func = getattr(__main__, function, None)
-	    if (callable(func)):
-		args = {} if len(params) == 1 else params[1].split(";")
-		print func(*args),
-	    else:
-		print "[CGI4LCD] Function '%s' not found\n" % function,
+        params = sys.argv[1].split("#")
+        function = params[0]
     
-    sys.exit()
+        if (function):
+            func = getattr(__main__, function, None)
+            if (callable(func)):
+                args = {} if len(params) == 1 else params[1].split(";")
+                print(func(*args))
+            else:
+                print("[CGI4LCD] Function '%s' not found\n" % function)
+        
+        sys.exit()
