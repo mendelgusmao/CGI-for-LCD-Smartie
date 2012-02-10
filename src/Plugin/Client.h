0c5becd10eb37898df6d3427063169a88525e58d
MendelGusmao <mendelsongusmao@gmail.com>
2012-02-10 17:07:22 -0200

    No need to change extensions of headers

---
+++ src/Plugin/Client.h
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+using namespace std;
+
+class Client {
+
+public:
+
+    static string _app_path;
+    static string _scripts_path;
+    static string _ini_file;
+    static unsigned int _port;
+    static unsigned int _execution_interval;
+    static unsigned int _execution_timeout;
+    static int _refresh_interval;
+    static string _default_extension;
+
+    static void start();
+    static string execute(string &script, const string &parameters, bool version=false, bool do_not_queue=false);
+    static string request(const string &interpreter, const string &arguments, unsigned int interval, unsigned int timeout, bool do_not_queue);
+    static string format_command(const string &command_template, const map<string, string> vars);
+
+};
+
+#endif // CLIENT_H
