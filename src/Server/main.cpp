e1344618c30db50e3ebb89bd3ea939c2a56f927c
MendelGusmao <mendelsongusmao@gmail.com>
2012-02-10 16:59:21 -0200

    Uppercasing first letter of class names (code)

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -1,15 +1,15 @@
 #include "stdafx.h"
-#include "server.h"
-#include "utils.h"
+#include "Server.h"
+#include "Utils.h"
 
 using boost::lexical_cast;
 
 int main(int argc, char* argv[]) {
 
-    string ini_file(utils::app_path() + "\\..\\scripts\\cgi4lcd.ini");
-    unsigned int port = lexical_cast<unsigned int>(utils::ini_read(ini_file, "cgi4lcd.port", "65432"));
-    bool show_window = utils::ini_read(ini_file, "cgi4lcd.show_window", "0") == "1";
-    bool add_and_run = utils::ini_read(ini_file, "cgi4lcd.add_and_run", "1") == "1";
+    string ini_file(Utils::app_path() + "\\..\\scripts\\cgi4lcd.ini");
+    unsigned int port = lexical_cast<unsigned int>(Utils::ini_read(ini_file, "cgi4lcd.port", "65432"));
+    bool show_window = Utils::ini_read(ini_file, "cgi4lcd.show_window", "0") == "1";
+    bool add_and_run = Utils::ini_read(ini_file, "cgi4lcd.add_and_run", "1") == "1";
 
     if (!show_window) {
         HWND hWnd = GetConsoleWindow();
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
 
     try {
         boost::asio::io_service io_service;
-        server s(io_service, port, add_and_run);
+        Server s(io_service, port, add_and_run);
         io_service.run();
     }
     catch (std::exception& e) {
