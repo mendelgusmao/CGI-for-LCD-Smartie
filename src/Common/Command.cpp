1b38ee660900e191f7863f7416c205aa96379832
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-02-10 16:33:05 -0200

    Uppercasing first letter of class names

---
+++ src/Common/Command.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "command.h"
+
+using std::string;
+
+command::command() :
+    timer(0),
+    cleanup_timer(0)
+{}
+
+string command::line() {
+    return executable + " " + arguments;
+}
+
+void command::run() {
+
+    char psBuffer[128];
+    FILE *iopipe;
+
+    iopipe = _popen(line().c_str(), "r");
+
+    if (iopipe == NULL) {
+        response = "[CGI4LCD] Error running...";
+    }
+    else {
+        response = "";
+
+        while(!feof(iopipe)) {
+            if(fgets(psBuffer, 128, iopipe) != NULL) {
+                response += string(psBuffer);
+            }
+        }
+        _pclose(iopipe);
+    }
+}
