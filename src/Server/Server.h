491c0cb9c0d2008be2dbe240a5688fbc8ae59bec
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-02-10 17:07:22 -0200

    No need to change extensions of headers

---
+++ src/Server/Server.h
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "Queue.h"
+
+#ifndef SERVER_H
+#define SERVER_H
+
+using boost::asio::ip::udp;
+
+class Server
+{
+
+public:
+    
+    boost::asio::io_service& _io_service;
+    udp::socket _socket;
+    Queue _queue;    
+    udp::endpoint _sender_endpoint;
+    bool _add_and_run;
+    enum { max_length = 1024 };
+    char _data[max_length];    
+    
+    Server(boost::asio::io_service& io_service, short port, bool add_and_run);
+    void handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd);
+    void handle_send_to(const boost::system::error_code& error, size_t bytes_sent);
+    void receive();
+
+};
+
+#endif // SERVER_H
