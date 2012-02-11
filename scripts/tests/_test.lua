12c6ad0350fde3eef810a1029a71a8bf9e61d770
Mendel Gusmao <mendelsongusmao@gmail.com>
2012-02-11 01:36:51 -0200

    WTF?

---
+++ scripts/tests/_test.lua
@@ -0,0 +1,16 @@
+-- $dll(cgi,1,test.lua,hello)
+function hello()
+    return("Hello, world!\n")
+end
+
+-- $dll(cgi,1,test.lua,say_hi#Mendel)
+function say_hi(who)
+    return("Hi, " .. who .. "\n")
+end
+
+-- $dll(cgi,1,test.lua,do_some_math#3;2;1)
+function do_some_math(a, b, c)
+    return(a * b + c)
+end
+
+main()
