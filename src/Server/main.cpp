d3da851682d10b395c7893df83537ffbae10aefa
MendelGusmao <mendelsongusmao@gmail.com>
2012-02-28 21:35:09 -0300

    Removing unnecessary couts

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -8,13 +8,12 @@ int main(int argc, char* argv[]) {
 
     string ini_file(Utils::app_path() + "\\..\\scripts\\cgi4lcd.ini");
     unsigned int port = lexical_cast<unsigned int>(Utils::ini_read(ini_file, "cgi4lcd.port", "65432"));
-    bool show_window = Utils::ini_read(ini_file, "cgi4lcd.show_window", "0") == "1";
     bool add_and_run = Utils::ini_read(ini_file, "cgi4lcd.add_and_run", "1") == "1";
 
-    if (!show_window) {
-        HWND hWnd = GetConsoleWindow();
-        ShowWindow( hWnd, SW_HIDE );
-    }
+#ifndef DEBUG
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow(hWnd, SW_HIDE);
+#endif
 
     try {
         boost::asio::io_service io_service;
