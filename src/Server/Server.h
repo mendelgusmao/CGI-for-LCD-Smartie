48fd9d4a624b46f350a275fea4037cd3dcdaae82
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-21 13:05:48 -0300

    Implemented 'max_threads' configuration

--- src/Server/Server.h
+++ src/Server/Server.h
@@ -18,7 +18,7 @@ public:
     enum { max_length = 1024 };
     char _data[max_length];    
     
-    Server(boost::asio::io_service& io_service, short port);
+    Server(boost::asio::io_service& io_service, short port, unsigned int max_threads);
     void handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd);
     void handle_send_to(const boost::system::error_code& error, size_t bytes_sent);
     void receive();
