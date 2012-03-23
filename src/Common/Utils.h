e13b54c6e0ebfe611bb9e37da3d576b69ad07193
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-23 10:52:50 -0300

    Adding helper functions

--- src/Common/Utils.h
+++ src/Common/Utils.h
@@ -12,6 +12,7 @@ public:
     static string ini_read(const string ini_file, const string key, const string default_value);
     static string app_path();
     static wstring s2ws(const string& s);
+    static void cls();
 
 };
 
