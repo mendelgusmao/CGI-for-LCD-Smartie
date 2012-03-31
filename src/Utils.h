ed06e723c1fe09e42059e765caf9ad054d86d1c9
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-31 18:36:28 -0300

    Removing unused methods; Simplifying configuration

--- src/Utils.h
+++ src/Utils.h
@@ -11,8 +11,6 @@ public:
 
     static string ini_read(const string ini_file, const string key, const string default_value);
     static string app_path();
-    static wstring s2ws(const string& s);
-    static void cls();
 
 };
 
